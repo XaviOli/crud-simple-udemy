5edc9e85f746b7674d2b5a3e45b5750157faae5f
Javier Diaz <javier.diazoliviero@gmail.com>
2022-10-10 17:08:29 +0200

    Método map

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Proyecto para poner en práctica conocimientos de REACT & Bootstrap 5
 En la etiqueta formulario añadimos los botones con la className para Bootstrap
 
 ![Link imagen](./src/img/crud-2.png)
+
+## `ID aleatorios`
+
+Instalando en la terminal npm i nanoid para generar números aleatorios de identificadores únicos dentro del array de objetos.
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 import React from "react";
+import { nanoid } from 'nanoid';
 
 function App() {
+
+  const [tarea, setTarea] = React.useState('')
+  const [tareas, setTareas] = React.useState([])
+
+  const agregarTarea = e => {
+    e.preventDefault()
+
+    if(!tarea.trim()){
+      console.log('Elemento vacío')
+      return
+    }
+    console.log(tarea)
+
+    setTareas([
+      ...tareas,
+      {id: nanoid(), NombreTarea: tarea}
+    ])
+
+    setTarea('')
+  }
+
   return (
     <div className="container mt-5">
       <h1 className="text-center" >CRUD Simple</h1>
@@ -9,20 +31,28 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la tarea</span>
-              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-              <button className="btn btn-warning btn-sm float-right">Editar</button>
+
+            {
+              tareas.map(item => (
+                <li className="list-group-item" key={item.id}>
+                <span className="lead">{item.NombreTarea}</span>
+                <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                <button className="btn btn-warning btn-sm float-right">Editar</button>
             </li>
+              ))
+            }
+
           </ul>
         </div>
         <div className="col-4">
           <h4 className="text-center">Formulario</h4>
-          <form>
+          <form onSubmit={ agregarTarea }>
             <input
               type="text"
               className="form-control mb-2"
               placeholder="Añade tarea"
+              onChange={ e => setTarea(e.target.value)}
+              value={tarea}
             />
             <button className="btn btn-dark btn-block" type="submit">Agregar</button>
           </form>
