e9ce3dc8e9420ad742de73ede4f42ab6ddb71dc5
Javier Diaz <javier.diazoliviero@gmail.com>
2022-10-10 16:18:40 +0200

    Listas de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container mt-5">
       <h1 className="text-center" >CRUD Simple</h1>
+      <hr/>
+      <div className="row">
+        <div className="col-8">
+          Listas de tareas
+        </div>
+        <div className="col-4">
+          Formulario
+        </div>
+      </div>
     </div>
   );
 }
