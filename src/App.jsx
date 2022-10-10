57e8c70cd4ad01fd6454f7d0b9ced1bb698432b0
Javier Diaz <javier.diazoliviero@gmail.com>
2022-10-10 16:14:10 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center" >CRUD Simple</h1>
+    </div>
+  );
+}
+
+export default App;
